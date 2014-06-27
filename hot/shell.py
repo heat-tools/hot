0cc55f3ad752157c8c19f3077b8bad6e560e56d7
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-06-27 13:26:05 -0500

    Changing name of the test to fabric

--- hot/shell.py
+++ hot/shell.py
@@ -100,8 +100,8 @@ def run_resource_tests(hc, stack_id, resource_tests):
 
     for test in resource_tests['tests']:
         test_name = test.keys()[0]
-        if test[test_name]['envassert']:
-            run_envassert_tasks(test_name, test[test_name])
+        if test[test_name]['fabric']:
+            run_fabric_tasks(test_name, test[test_name])
 
     delete_file(ssh_key_file, "Deleting ssh_private_key file '%s'." %
                 ssh_key_file)
@@ -132,19 +132,19 @@ def update_dict(items, outputs):
     return items
 
 
-def run_envassert_tasks(test_name, test):
-    """Setup fabric environment and run envassert script"""
-    env_setup = test['envassert']
+def run_fabric_tasks(test_name, test):
+    """Setup fabric environment and run fabric script"""
+    env_setup = test['fabric']
     if env_setup['env']:
         fab_file = env_setup['env']['fabfile']
-        print "  Preparing environtment to run envassert tests:"
+        print "  Preparing environtment to run fabric tests:"
         for k, v in env_setup['env'].iteritems():
             print "    Setting env['%s'] to %s" % (k, v)
             env[k] = v
         mod_name = os.path.splitext(os.path.basename(fab_file))[0]
         mod = imp.load_source(mod_name, fab_file)
         for task in env_setup['env']['tasks']:
-            print "  Run envassert test '%s', task '%s' on: %s" % (test_name,
+            print "  Run fabric test '%s', task '%s' on: %s" % (test_name,
                                                                    task,
                                                                    env.hosts)
             fabric.tasks.execute(getattr(mod, task))
