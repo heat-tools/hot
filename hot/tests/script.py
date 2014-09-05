1f4a0a2011d6838b59d8bbffb15655a9aa7e15a1
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-09-05 15:30:39 -0500

    Added hosts file override capabilities

--- hot/tests/script.py
+++ hot/tests/script.py
@@ -1,16 +1,34 @@
 import os
 import subprocess
 
-from hot.utils import string
 from hot.utils import files
+from hot.utils import hosts
+from hot.utils import string
 
 
 def run_script(test_name, test):
     """Setup environment and run a script"""
     script_setup = test['script']
     if "hosts" in script_setup:
-        # TODO: Set hosts File
-        print "TODO: Setup hosts file"
+        if os.name == 'nt':
+            hosts_path = os.path.join(os.environ['SYSTEMROOT'],
+                                      'system32/drivers/etc/hosts')
+        elif os.name == 'posix':
+            hosts_path = '/etc/hosts'
+        h = hosts.Hosts(hosts_path)
+        for entry in script_setup['hosts']:
+            if "ip" in entry:
+                ip = entry["ip"]
+                if "hostnames" in entry:
+                    for hostname in entry['hostnames']:
+                        print "  Setting host entry '%s\t%s'" % (ip, hostname)
+                        if isinstance(hostname, str):
+                            h.set_one(hostname, ip)
+                        elif isinstance(hostname, list):
+                            h.set_all(hostname, ip)
+            else:
+                raise TypeError
+        h.write(hosts_path)
 
     # Write Files
     if "files" in script_setup:
--- hot/utils/__init__.py
+++ hot/utils/__init__.py
@@ -1,6 +1,7 @@
 """Misc utilities that can be used across the project"""
 
 import hot.utils.files
+import hot.utils.hosts
 import hot.utils.repo
 import hot.utils.timeout
 import hot.utils.token
