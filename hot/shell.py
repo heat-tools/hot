1ba9b6b7b6950d292766683175ac4e3cb6b4396d
Justin Phelps <linuturk@onitato.com>
2014-10-23 15:47:40 -0500

    solves our CI badge problem, and now pulls instructions from rackspace.yaml

--- hot/shell.py
+++ hot/shell.py
@@ -40,22 +40,46 @@ def verify_environment_vars(variables):
 
 @alias('docs')
 @arg('--template', default='.catalog')
+@arg('--badge', default='None', help='Badge to add to the top of the output.')
 def do_create_docs(args):
     """Generate the basics for the README.md based on the information in a
     given template.
     """
     verified_template_directory = hot.utils.repo.check(args)
     template_attr = getattr(args, 'template')
+    badge_attr = getattr(args, 'badge')
     path_to_template = os.path.join(verified_template_directory, template_attr)
+    path_to_raxtemplate = os.path.join(verified_template_directory, 'rackspace.yaml')
     try:
         raw_template = get_raw_yaml_file(args, file_path=path_to_template)
         validated_template = hot.utils.yaml.load(raw_template)
+        raw_raxtemplate = get_raw_yaml_file(args, file_path=path_to_raxtemplate)
+        validated_raxtemplate = hot.utils.yaml.load(raw_raxtemplate)
     except StandardError as exc:
         sys.exit(exc)
+    # Set necessary variables for CI badges based on rackspace.yaml information
+    if 'github-organization' in validated_raxtemplate:
+        github_org = validated_raxtemplate['github-organization']
+    else:
+        github_org = 'rackspace-orchestration-templates'
+    if 'github-repository-name' in validated_raxtemplate:
+        github_repo = validated_raxtemplate['github-repository-name']
+    else:
+        github_repo = ''
+    # If the --badge arg is passed let's print the badges out first
+    if badge_attr:
+        if badge_attr == 'circle':
+            url_prefix = "https://circleci.com/gh/"
+            print "[![Circle CI]({0}{1}/{2}.png?style=badge)]({0}{1}/{2})".format(url_prefix, github_org, github_repo)
+        else:
+            pass
     # print validated_template['parameters']
     if 'description' in validated_template:
         print "Description\n===========\n\n%s\n" % \
               validated_template['description']
+    # Pull out the instructions from rackspace.yaml
+    if 'instructions' in validated_raxtemplate:
+        print "Instructions\n===========\n\n{0}\n".format(validated_raxtemplate['instructions'])
     if 'resources' in validated_template:
         resources = get_resource_types(validated_template['resources'])
         print "Requirements\n============\n* A Heat provider that supports th"\
