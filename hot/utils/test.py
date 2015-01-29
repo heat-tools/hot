39776da20b7e0bf38b119701a696980b7a71a514
Justin Phelps <linuturk@onitato.com>
2015-01-28 18:04:48 -0600

    nuke trailing whitespace

--- hot/utils/test.py
+++ hot/utils/test.py
@@ -19,29 +19,29 @@ def get_artifacts(artifacts, envvar='CIRCLE_ARTIFACTS'):
     for artifact in artifacts:
         target = directory + "/%(host)s/%(path)s"
         try:
-            get(artifact, target)                                                                                                    
-        except:                                                                                                                      
-            pass                                                                                                                     
-                                                                                                                                     
-                                                                                                                                     
-def http_check(site, string):                                                                                                        
-    """Search the html of site with the string provided."""                                                                          
-    with hide('running', 'stdout'):                                                                                                  
-        wget_cmd = "wget --quiet --output-document - --no-check-certificate"                                                         
-        homepage = run("{0} {1}".format(wget_cmd, site))                                                                             
-        if re.search(string, homepage):                                                                                              
-            return True                                                                                                              
-        else:                                                                                                                        
-            return False                                                                                                             
-                                                                                                                                     
-                                                                                                                                     
-def local_http_check(url, string):                                                                                                   
-    """Run a local http request and search the html for the provided string."""                                                      
-    try:                                                                                                                             
-        r = requests.get(url)                                                                                                        
-        if re.search(string, r.content):                                                                                             
-            return True                                                                                                              
-        else:                                                                                                                        
-            return False                                                                                                             
-    except:                                                                                                                          
+            get(artifact, target)
+        except:
+            pass
+
+
+def http_check(site, string):
+    """Search the html of site with the string provided."""
+    with hide('running', 'stdout'):
+        wget_cmd = "wget --quiet --output-document - --no-check-certificate"
+        homepage = run("{0} {1}".format(wget_cmd, site))
+        if re.search(string, homepage):
+            return True
+        else:
+            return False
+
+
+def local_http_check(url, string):
+    """Run a local http request and search the html for the provided string."""
+    try:
+        r = requests.get(url)
+        if re.search(string, r.content):
+            return True
+        else:
+            return False
+    except:
         return False
