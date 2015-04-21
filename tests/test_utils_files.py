b668ce5e502db2beeb12faf798703c9a01c3c777
Justin Phelps <linuturk@onitato.com>
2015-04-21 16:34:06 -0500

    adding tests for the files utils

---
+++ tests/test_utils_files.py
@@ -0,0 +1,31 @@
+import unittest
+import os
+from hot.utils import files
+
+
+class TestUtilFiles(unittest.TestCase):
+
+    def setUp(self):
+        self.wf = "writefile.temp"
+        self.string = "test value"
+        self.lst = ["delfile.temp"]
+        self.dictionary = {}
+
+    def test_write_file(self):
+        self.assertIsNone(files.write_file(self.wf, self.string))
+        os.remove(self.wf)
+
+    def test_write_file_invalid(self):
+        self.assertRaises(TypeError, files.write_file, self.wf, self.lst)
+        os.remove(self.wf)
+
+    def test_delete_file(self):
+        for f in self.lst:
+            open(f, 'a').close()
+        self.assertIsNone(files.delete_file(self.lst))
+
+    def test_delete_file_invalid(self):
+        self.assertRaises(TypeError, files.delete_file, self.dictionary)
+
+if __name__ == "__main__":
+    unittest.main()
